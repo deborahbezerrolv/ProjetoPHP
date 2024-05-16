8719ba5b1e2de280237c25485b42ccafcdab9fa8
deborah oliveira <deborah.oliveira50@etec.sp.gov.br>
2024-05-16 20:07:17 -0300

    160524

--- cabecalho.php
+++ cabecalho.php
@@ -17,11 +17,18 @@
             text-align: center; 
         }
         .col-md-4 {
-          width: 70%; 
-            height: 40vh; 
+          width: 50%; 
+            height: 20vh; 
             display: flex;
             justify-content: center; 
             align-items: center; 
+        }
+        .col-12{
+          width: 20%;
+          height: 20%;
+          display: flex;
+          justify-content: left;
+          align-items: center;
         }
     </style>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
--- form2.php
+++ form2.php
@@ -1,83 +1,60 @@
 <form class="row g-3 needs-validation" novalidate>
-  <div class="col-md-4">
-    <label for="validationCustom01" class="form-label">Nome do Dono</label>
-    <input type="text" class="form-control" id="validationCustom01" value="Mark" required>
-    <div class="valid-feedback">
-    
+  <div class="col-md-4 position-relative">
+    <label for="validationTooltip01" class="form-label">Nome do Dono:</label>
+    <input type="text" class="form-control" id="validationTooltip01" value="" required>
+    <div class="valid-tooltip">
     </div>
   </div>
-  <div class="col-md-4">
-    <label for="validationCustom02" class="form-label">Documento</label>
-    <input type="text" class="form-control" id="validationCustom02" value="Otto" required>
-    <div class="valid-feedback">
-      Looks good!
+  <div class="col-md-4 position-relative">
+    <label for="validationTooltip02" class="form-label">Documento:</label>
+    <input type="text" class="form-control" id="validationTooltip02" value="" required>
+    <div class="valid-tooltip">
     </div>
   </div>
-  <div class="col-md-4">
-    <label for="validationCustomUsername" class="form-label">Animal</label>
-    <div class="input-group has-validation">
-      <span class="input-group-text" id="inputGroupPrepend"></span>
-      <input type="text" class="form-control" id="validationCustomUsername" aria-describedby="inputGroupPrepend" required>
-      <div class="invalid-feedback">
-        Please choose a username.
-      </div>
+  <div class="col-md-4 position-relative">
+    <label for="validationTooltip02" class="form-label">Nome do Animal:</label>
+    <input type="text" class="form-control" id="validationTooltip02" value="" required>
+    <div class="valid-tooltip">
     </div>
   </div>
-  <div class="col-md-6">
-    <label for="validationCustom03" class="form-label">Idade do Animal</label>
-    <input type="text" class="form-control" id="validationCustom03" required>
-    <div class="invalid-feedback">
-      Please provide a valid city.
+  </div>
+  <div class="col-md-6 position-relative">
+    <label for="validationTooltip03" class="form-label">Espécie do Animal:</label>
+    <input type="text" class="form-control" id="validationTooltip03" required>
+    <div class="invalid-tooltip">
     </div>
   </div>
-  <div class="col-md-3">
-    <label for="validationCustom04" class="form-label">Alergia</label>
-    <select class="form-select" id="validationCustom04" required>
-      <option selected disabled value="">Choose...</option>
-      <option>...</option>
-    </select>
-    <div class="invalid-feedback">
-      Please select a valid state.
+  <div class="col-md-6 position-relative">
+    <label for="validationTooltip03" class="form-label">Alergia a algum remedio?</label>
+    <input type="text" class="form-control" id="validationTooltip03" required>
+    <div class="invalid-tooltip">
     </div>
   </div>
-  <div class="col-md-3">
-    <label for="validationCustom05" class="form-label">Cirurgia</label>
-    <input type="text" class="form-control" id="validationCustom05" required>
-    <div class="invalid-feedback">
-      Please provide a valid zip.
-    </div>
-    <div class="col-md-4">
-    <label for="validationCustom05" class="form-label">Telefone</label>
-    <input type="text" class="form-control" id="validationCustom05" required>
-    <div class="invalid-feedback">
-      Please provide a valid zip.
-    </div>
-    <div class="col-md-3">
-    <label for="validationCustom05" class="form-label">Data</label>
-    <input type="text" class="form-control" id="validationCustom05" required>
-    <div class="invalid-feedback">
-      Please provide a valid zip.
+  <div class="col-md-6 position-relative">
+    <label for="validationTooltip03" class="form-label">Cirurgia nos ultimos tres anos?</label>
+    <input type="text" class="form-control" id="validationTooltip03" required>
+    <div class="invalid-tooltip">
     </div>
   </div>
-  <div class="col-md-3">
-    <label for="validationCustom05" class="form-label">Horario</label>
-    <input type="text" class="form-control" id="validationCustom05" required>
-    <div class="invalid-feedback">
-      Please provide a valid zip.
+  <div class="col-md-6 position-relative">
+    <label for="validationTooltip03" class="form-label">Telefone: </label>
+    <input type="text" class="form-control" id="validationTooltip03" required>
+    <div class="invalid-tooltip">
     </div>
-  <div class="col-12">
-    <div class="form-check">
-      <input class="form-check-input" type="checkbox" value="" id="invalidCheck" required>
-      <label class="form-check-label" for="invalidCheck">
-        Agree to terms and conditions
-      </label>
-      <div class="invalid-feedback">
-        You must agree before submitting.
-      </div>
+  </div>
+  <div class="col-md-6 position-relative">
+    <label for="validationTooltip03" class="form-label">Data da Consulta:</label>
+    <input id="date" type="date" />
+    <div class="invalid-tooltip">
     </div>
-    
+  </div>
+  <div class="col-md-6 position-relative">
+    <label for="validationTooltip03" class="form-label">Horário da Consulta:</label>
+    <input type="time">
+    <div class="invalid-tooltip">
+    </div>
   </div>
   <div class="col-12">
-    <button class="btn btn-primary" type="submit">Submit form</button>
+    <button class="btn btn-primary" type="submit">Confirmar Agendamento</button>
   </div>
 </form>
