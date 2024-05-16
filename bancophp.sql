8395c7369d167aad86d2ff089a86a0768d35a9ce
deborah oliveira <deborah.oliveira50@etec.sp.gov.br>
2024-05-16 19:26:49 -0300

    1605

--- bancophp.sql
+++ bancophp.sql
@@ -3,21 +3,6 @@ create database veterinario;
 use veterinario;
 
 create table login(
-usuario varchar (45) not null unique,
-senha int (16) not null
-);
-
-select * from login;
-
-insert into login (usuario, senha) values ('admin', '1234');
-
-insert into login (usuario, senha) values ('colaboradorvet', '4321');
-
-alter table login add column id int (15) not null;
-
-drop table login;
-
-create table login(
 id int (15) not null unique,
 usuario varchar (45) not null unique,
 senha int (16) not null
--- form2.php
+++ form2.php
@@ -1,22 +1,22 @@
 <form class="row g-3 needs-validation" novalidate>
   <div class="col-md-4">
-    <label for="validationCustom01" class="form-label">First name</label>
+    <label for="validationCustom01" class="form-label">Nome do Dono</label>
     <input type="text" class="form-control" id="validationCustom01" value="Mark" required>
     <div class="valid-feedback">
-      Looks good!
+    
     </div>
   </div>
   <div class="col-md-4">
-    <label for="validationCustom02" class="form-label">Last name</label>
+    <label for="validationCustom02" class="form-label">Documento</label>
     <input type="text" class="form-control" id="validationCustom02" value="Otto" required>
     <div class="valid-feedback">
       Looks good!
     </div>
   </div>
   <div class="col-md-4">
-    <label for="validationCustomUsername" class="form-label">Username</label>
+    <label for="validationCustomUsername" class="form-label">Animal</label>
     <div class="input-group has-validation">
-      <span class="input-group-text" id="inputGroupPrepend">@</span>
+      <span class="input-group-text" id="inputGroupPrepend"></span>
       <input type="text" class="form-control" id="validationCustomUsername" aria-describedby="inputGroupPrepend" required>
       <div class="invalid-feedback">
         Please choose a username.
@@ -24,14 +24,14 @@
     </div>
   </div>
   <div class="col-md-6">
-    <label for="validationCustom03" class="form-label">City</label>
+    <label for="validationCustom03" class="form-label">Idade do Animal</label>
     <input type="text" class="form-control" id="validationCustom03" required>
     <div class="invalid-feedback">
       Please provide a valid city.
     </div>
   </div>
   <div class="col-md-3">
-    <label for="validationCustom04" class="form-label">State</label>
+    <label for="validationCustom04" class="form-label">Alergia</label>
     <select class="form-select" id="validationCustom04" required>
       <option selected disabled value="">Choose...</option>
       <option>...</option>
@@ -41,12 +41,30 @@
     </div>
   </div>
   <div class="col-md-3">
-    <label for="validationCustom05" class="form-label">Zip</label>
+    <label for="validationCustom05" class="form-label">Cirurgia</label>
+    <input type="text" class="form-control" id="validationCustom05" required>
+    <div class="invalid-feedback">
+      Please provide a valid zip.
+    </div>
+    <div class="col-md-4">
+    <label for="validationCustom05" class="form-label">Telefone</label>
+    <input type="text" class="form-control" id="validationCustom05" required>
+    <div class="invalid-feedback">
+      Please provide a valid zip.
+    </div>
+    <div class="col-md-3">
+    <label for="validationCustom05" class="form-label">Data</label>
     <input type="text" class="form-control" id="validationCustom05" required>
     <div class="invalid-feedback">
       Please provide a valid zip.
     </div>
   </div>
+  <div class="col-md-3">
+    <label for="validationCustom05" class="form-label">Horario</label>
+    <input type="text" class="form-control" id="validationCustom05" required>
+    <div class="invalid-feedback">
+      Please provide a valid zip.
+    </div>
   <div class="col-12">
     <div class="form-check">
       <input class="form-check-input" type="checkbox" value="" id="invalidCheck" required>
@@ -57,6 +75,7 @@
         You must agree before submitting.
       </div>
     </div>
+    
   </div>
   <div class="col-12">
     <button class="btn btn-primary" type="submit">Submit form</button>
