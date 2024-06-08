f76723190a4a8b07698ceb2eb3d0eb453e88a2dd
Deborah Oliveira <143664769+deborahbezerrolv@users.noreply.github.com>
2024-06-08 12:44:15 -0300

    alteracoesmenu

--- navbar.php
+++ navbar.php
@@ -1,5 +1,6 @@
 <nav class="navbar bg-body-tertiary">
   <div class="container-fluid">
+  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTirmy7zgkqbUjwh7w9WLWJu8WPUVpOzSVZhQ&s" class="rounded float-start" alt="logo1">
     <a class="navbar-brand" href="#">MADAGASCAR</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
