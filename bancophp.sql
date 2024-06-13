9b0d5be0048aea340fe0395c29db367ab461c91d
Deborah Oliveira <deborah.oliveira50@etec.sp.gov.br>
2024-06-13 19:38:00 -0300

    atualizacao banco de dados

--- bancophp.sql
+++ bancophp.sql
@@ -1,14 +1,11 @@
-create database veterinario;
+create database grupo01php;
 
-use veterinario;
+use grupo01php;
 
-create table login(
-id int (15) not null unique,
+create table cliente(
+id int (11) not null primary key auto_increment,
 usuario varchar (45) not null unique,
 email varchar (45) not null unique,
 senha varchar (16) not null
 );
 
-
-insert into login (id, usuario, email, senha) values ('13898','admin', 'admin@gmail.com', '1234');
-insert into login (id, usuario, email, senha) values ('13899','colaboradorvet', 'colabvet@gmail.com' '4321');
